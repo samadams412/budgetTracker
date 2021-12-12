0028e028277cb25e816bad6df9117f2f0dfadaa6
Samuel Adams <samueladams412@gmail.com>
2021-12-11 18:52:18 -0600

    fixing heroku offline

--- server.js
+++ server.js
@@ -16,11 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget_db',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
